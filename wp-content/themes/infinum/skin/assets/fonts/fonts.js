19f78714e3fbb4513a5bfad182cfb0d1f56770c3
Tihi321 <knox@dr.com>
2018-05-20 09:39:57 +0200

    Removed webfonts, added MuseoSansRounded

--- wp-content/themes/infinum/skin/assets/fonts/fonts.js
+++ wp-content/themes/infinum/skin/assets/fonts/fonts.js
@@ -2,3 +2,10 @@ import './icomoon.eot';
 import './icomoon.svg';
 import './icomoon.ttf';
 import './icomoon.woff';
+
+import './MuseoSansRounded-100.otf';
+import './MuseoSansRounded-300.otf';
+import './MuseoSansRounded-500.otf';
+import './MuseoSansRounded-700.otf';
+import './MuseoSansRounded-900.otf';
+import './MuseoSansRounded-1000.otf';
